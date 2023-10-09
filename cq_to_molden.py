2d2ebe20921b0f9fe182010271e0a444751aeafb
smgarner229 <smgarner@berkeley.edu>
2023-10-09 14:38:03 -0700

    Split writing to own functions, modify main procedure

--- cq_to_molden.py
+++ cq_to_molden.py
@@ -340,26 +340,34 @@ def skim_cq_out(filename):
           read_mos(f1,mol,mol.pbasis,True)
         else:
           read_mos(f1,mol,mol.ebasis)
-
+  return mol
+  
+def write_electronic_MOs(mol,basename,extension="_electronic_MOs.molden"):
+  filename = basename + extension
   # Make a molecule with the classical positions to plot electronic orbitals
   pyscfmol = gto.Mole(atom=mol.geometry,charge=mol.charge,spin=mol.mult)
   pyscfmol.basis = mol.ebasis.basis
   pyscfmol.build()
-  with open(basename+"_electronic_MOs.molden",'w') as f1:
+  with open(filename,'w') as f1:
     molden.header(pyscfmol,f1)
     molden.orbital_coeff(pyscfmol,f1,mol.mo_coeff,ene=mol.mo_energy)
 
+def write_protonic_MOs(mol,basename,extension="_protonic_MOs.molden"):
   # Make a dummy molecule to plot the protonic orbitals
   # probably need to pad the protonic molecular orbitals with 0's and ghost
   # basis centers on the rest of the atoms
-  if NEO:
-    pyscfpmol = gto.Mole(atom=mol.p_geometry())
-    pyscfpmol.basis = mol.pbasis.basis
-    pyscfpmol.build()
-    with open(basename+"_protonic_MOs.molden",'w') as f1:
-      molden.header(pyscfpmol,f1)
-      molden.orbital_coeff(pyscfpmol,f1,mol.pmo_coeff,ene=mol.pmo_energy)
-  
+  filename = basename+extension
+  pyscfpmol = gto.Mole(atom=mol.p_geometry(),charge=mol.charge,spin=mol.mult)
+  pyscfpmol.basis = mol.pbasis.basis
+  pyscfpmol.build()
+  with open(filename,'w') as f1:
+    molden.header(pyscfpmol,f1)
+    molden.orbital_coeff(pyscfpmol,f1,mol.pmo_coeff,ene=mol.pmo_energy)
+ 
 
 if __name__ == "__main__":
-  skim_cq_out(sys.argv[1])
+  basename = sys.argv[1].split('.')[0]
+  mol = skim_cq_out(sys.argv[1])
+  write_electronic_MOs(mol,basename)
+  if mol.is_NEO:
+    write_protonic_MOs(mol,basename)
